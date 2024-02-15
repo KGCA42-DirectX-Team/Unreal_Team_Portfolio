63f322d7086b7903ff4d807c76826c0562db86dd
bank2014 <bank2014@naver.com>
2024-02-16 00:57:27 +0900

    ADD 556 bullet

---
+++ Source/BrutalTakedownSquad/Public/Actor/ABTSProjectile.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
+#include "ABTSProjectile.generated.h"
+
+class UNiagaraSystem;
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class BRUTALTAKEDOWNSQUAD_API ABTSProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ABTSProjectile();
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	FGameplayEffectSpecHandle DamageEffectSpecHandle;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Destroyed() override;
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+private:
+
+	UPROPERTY(EditDefaultsOnly)
+	float LifeSpan = 15.0f;
+	bool bHit = false;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
