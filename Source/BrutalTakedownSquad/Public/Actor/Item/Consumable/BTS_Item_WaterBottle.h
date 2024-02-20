eb179b1573b86524dca3b531bd9c6f6435cc6553
bank2014 <bank2014@naver.com>
2024-02-20 22:19:58 +0900

    ADD water bottle (consumable)

---
+++ Source/BrutalTakedownSquad/Public/Actor/Item/Consumable/BTS_Item_WaterBottle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/Item/BTS_Item.h"
+#include "BTS_Item_WaterBottle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRUTALTAKEDOWNSQUAD_API ABTS_Item_WaterBottle : public ABTS_Item
+{
+	GENERATED_BODY()
+	
+};
