33f74b4ead9aaa3990686faa153fc9cfabd84f31
bank2014 <bank2014@naver.com>
2024-02-20 19:15:33 +0900

    ADD Weapon Gameplay ability base class

---
+++ Source/BrutalTakedownSquad/Public/AbilitySystem/Ability/Weapon/BTS_GA_Weapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Ability/BTSGameplayAbility.h"
+#include "BTS_GA_Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRUTALTAKEDOWNSQUAD_API UBTS_GA_Weapon : public UBTSGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
