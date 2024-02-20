2d6c5ba2ec9e206155da54e29504dd995762fc99
bank2014 <bank2014@naver.com>
2024-02-20 19:27:53 +0900

    ADD GA Weapon aim down sight

---
+++ Source/BrutalTakedownSquad/Public/AbilitySystem/Ability/Weapon/BTS_GA_Weapon_ADS.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Ability/Weapon/BTS_GA_Weapon.h"
+#include "BTS_GA_Weapon_ADS.generated.h"
+
+/// <summary>
+/// Weapon Game Play Ability Class.
+/// </summary>
+/// @author ±è¼±¿ì
+/// @date last change 2024/02/20
+UCLASS()
+class BRUTALTAKEDOWNSQUAD_API UBTS_GA_Weapon_ADS : public UBTS_GA_Weapon
+{
+	GENERATED_BODY()
+	
+};
