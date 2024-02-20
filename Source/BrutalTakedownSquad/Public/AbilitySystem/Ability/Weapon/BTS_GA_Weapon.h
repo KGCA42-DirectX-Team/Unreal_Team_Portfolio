ae69d475b2042ecc50ef211edf555db7e653a9fd
bank2014 <bank2014@naver.com>
2024-02-20 19:16:04 +0900

    ADD comments

--- Source/BrutalTakedownSquad/Public/AbilitySystem/Ability/Weapon/BTS_GA_Weapon.h
+++ Source/BrutalTakedownSquad/Public/AbilitySystem/Ability/Weapon/BTS_GA_Weapon.h
@@ -6,9 +6,11 @@
 #include "AbilitySystem/Ability/BTSGameplayAbility.h"
 #include "BTS_GA_Weapon.generated.h"
 
-/**
- * 
- */
+/// <summary>
+/// Weapon Game Play Ability Class.
+/// </summary>
+/// @author ±è¼±¿ì
+/// @date last change 2024/02/20
 UCLASS()
 class BRUTALTAKEDOWNSQUAD_API UBTS_GA_Weapon : public UBTSGameplayAbility
 {
